93fe9b0e42f93f4942b6dcc90fd36a08dd5d7e0b
Bethany Oestrich <bethany@oestrich.org>
2018-04-08 23:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ function updateObjectWithKeyAndValue (object, key, value){
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value){
   Object.assign({"key": value},recipes) 
+  return recipes
 }
 
