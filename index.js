7a3070e07a288f3b2e01c40f581a86ac1c201a16
Bethany Oestrich <bethany@oestrich.org>
2018-04-09 00:20:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value){
 
 function deleteFromObjectByKey(object, key)
 {
-  var newobj = Object.assign({key},recipes)
-  delete  newobj.key
-  return newobj;
+  var newObj = Object.assign({key},recipes)
+  delete newObj[key]
+  return object;
 }
 function destructivelyDeleteFromObjectByKey(object,key)
 {
